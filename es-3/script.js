6936d996f1dfd20eb6ad86e4e2f635a5e3307fb4
mouhcine <mouhcine15.00@gmail.com>
2022-05-11 10:24:06 +0200

    coorrezione

--- es-3/script.js
+++ es-3/script.js
@@ -3,27 +3,60 @@ const secondlevel = ["parola1-2", "parola2-2"];
 console.log(firstlevel);
 console.log(secondlevel);
 
+// for (let i = 0; i < firstlevel.length; i++) {
+//     const node = document.createElement("li");
+//     const textnode = document.createTextNode(firstlevel[i]);
+
+//     node.appendChild(textnode);
+
+//     document.getElementById("myList").appendChild(node);
+    
+//     for (let i = 0; i < secondlevel.length; i++) {
+//         const provaul = document.createElement("li");
+//         const node2 = document.createElement("ul");
+//         const textnode2 = document.createTextNode(secondlevel[i]);
+    
+//         node2.appendChild(provaul);
+//         provaul.appendChild(textnode2);
+    
+//         document.getElementById("myList").appendChild(node2);  
+//     };
+// };
+
+
+
+// selezione UL primo livello
+const firstLevUl = document.getElementById("myList");
+
+// livello 1
 for (let i = 0; i < firstlevel.length; i++) {
-    const node = document.createElement("li");
-    const textnode = document.createTextNode(firstlevel[i]);
+    // estraplo il contenuto iesimo del primo array
+    let firstLevContent = firstlevel[i];
+    // mi creo il LI di primo livello, iesimo
+    let firstLevLi = document.createElement("li");
 
-    node.appendChild(textnode);
+    // insierire il contenuto del LI, primo livello
+    firstLevLi.append(firstLevContent);
+    // inserisco il LI di primo livello creato nel UL di primo livello
+    firstLevUl.append(firstLevLi);
 
-    document.getElementById("myList").appendChild(node);
     
-    for (let i = 0; i < secondlevel.length; i++) {
-        const provaul = document.createElement("ul");
-        const node2 = document.createElement("li");
-        const textnode2 = document.createTextNode(secondlevel[i]);
+    // mi serve qui fuori sennò fa più ul annidati ogni li primario
+    let secondLevUl = document.createElement("ul");
+
+    for (let j = 0; j < secondlevel.length; j++) {
+        // estraplo il contenuto iesimo del secondo array
+        let secondLevContent = secondlevel[j];
+        // mi creo il LI di secondo livello, iesimo
+        let secondLevLi = document.createElement("li");
     
-        node2.appendChild(provaul);
-        provaul.appendChild(textnode2);
+        // insierire il contenuto del LI, secondo livello
+        secondLevLi.append(secondLevContent);
+        // inserisco il LI di primo livello creato nel UL di secondo livello
+        secondLevUl.append(secondLevLi);
+        // inserisco UL di secondo livello nel LI di primo livello
+        firstLevLi.append(secondLevUl);
     
-        document.getElementById("myList").appendChild(node2);
-        
     };
+
 };
-
-
-
-// firstlevel.append(secondlevel);
